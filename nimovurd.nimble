d40b91300cafddac77e538ee56751a2e2e73b799
Roj <ez@roj.im>
2023-02-10 13:32:32 +0300

    Initial commit

---
+++ nimovurd.nimble
@@ -0,0 +1,16 @@
+# Package
+
+version       = "0.1.0"
+author        = "Roj"
+description   = "A new awesome nimble package"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["nimovurd"]
+
+
+# Dependencies
+
+requires "nim >= 1.6.10"
+requires "httpbeast"
+requires "norm"
+requires "nimkov"
---
+++ src/nimovurd.nim
@@ -0,0 +1,49 @@
+import md5
+import options
+import strutils
+import std/with
+import nimkov/[generator, typedefs, validators]
+import norm/[model, pragmas, sqlite]
+import httpbeast
+import asyncdispatch
+
+type
+    GeneratedText* {.tableName: "generated_texts".} = ref object of Model
+        text* {.unique.}: string
+
+
+func newGeneratedText(text = ""): GeneratedText =
+    GeneratedText(text: text)
+
+
+proc getHandler(markov: MarkovGenerator, dbConn: DbConn): OnRequest =
+    return proc(req: Request): Future[void] {.gcsafe.} =
+        let options = MarkovGenerateOptions(attempts: 10000,
+                validator: defaultValidator());
+        let text = markov.generate(options).get
+
+        var generatedText = newGeneratedText($toMD5(text))
+
+        try:
+            with dbConn:
+                insert generatedText
+        except DbError as e:
+            if "UNIQUE constraint failed" in e.msg:
+                echo "duplicate"
+                req.send(Http404)
+                return
+            else:
+                raise
+
+        req.send(Http200, text, "Content-Type: text/plain; charset=UTF-8")
+
+when isMainModule:
+    var file = open("tweets/all")
+    let phrases = file.readAll().split("\n")
+    file.close()
+    let markov = newMarkov(phrases)
+
+    let dbConn = open("movurd.db", "", "", "")
+    dbConn.createTables(newGeneratedText())
+
+    run(getHandler(markov, dbConn), initSettings(numThreads = 1))
