4d71bb77122a719444691dadd7579f04eb7dc80d
Roj <ez@roj.im>
2023-02-10 14:32:47 +0300

    Rewrite the merge script accordingly

--- merge.ts
+++ merge.ts
@@ -1,29 +1,30 @@
-const files = [...Deno.readDirSync("./tweets")]
-  .filter((v) => v.isFile && v.name != "all")
-  .map((v) => `./tweets/${v.name}`);
+const files = [...Deno.readDirSync("./tweet_data/data")]
+  .map((v) => `./tweet_data/data/${v.name}`);
 
 const tweets = new Array<string>();
 
-const BLACKLIST = [/گوو/];
+const BLACKLIST = [/گوو/]; // TODO: improve this
 
 for (const file of files) {
-  tweets.push(
-    ...Deno.readTextFileSync(file)
-      .split("\n")
-      .filter((v) => v)
-      .map((v) => JSON.parse(v))
-      .map((v) => v.replace(/ك/g, "ک"))
-      .map((v) => v.replace(/ھ/g, "ه"))
-      .map((v) => v.replace(/[^قوەرتیئحۆپاسدفگهژکلزخجڤبنمڕێعشغڵچ \n]/g, ""))
-      .filter((v) => !BLACKLIST.some((e) => e.test(v)))
-      .map((v) => v.replace(/ {2,}/g, " "))
-      .map((v) => v.replace(/\n{2,}/g, "\n"))
-      .map((v) => v.trim()),
-  );
+  try {
+    tweets.push(
+      ...(Deno.readTextFileSync(file)
+        .split("\n")
+        .filter((v) => v)
+        .map((v) => JSON.parse(v))
+        .map((v) => v.rawContent) as string[])
+        .map((v) => v.replace(/ك/g, "ک"))
+        .map((v) => v.replace(/ھ/g, "ه"))
+        .map((v) => v.replace(/[^قوەرتیئحۆپاسدفگهژکلزخجڤبنمڕێعشغڵچ \n]/g, ""))
+        .filter((v) => !BLACKLIST.some((e) => e.test(v)))
+        .map((v) => v.replace(/ {2,}/g, " "))
+        .map((v) => v.replace(/\n{2,}/g, "\n"))
+        .map((v) => v.trim()),
+    );
+  } catch (err) {
+    console.warn("Failed to process", file, "because of the following error:");
+    console.error(err);
+  }
 }
 
-Deno.writeTextFileSync(
-  "./tweets/all",
-  [...new Set(tweets)]
-    .join("\n"),
-);
+Deno.writeTextFileSync("./tweets.txt", [...new Set(tweets)].join("\n"));
